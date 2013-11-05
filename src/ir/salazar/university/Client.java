ad0cd10b66b6fdb434a46b4fdc2480413fd7c3ce
mohi <mohibeyki@gmail.com>
2013-11-05 18:30:23 +0330

    updated somestuff

--- src/ir/salazar/university/Client.java
+++ src/ir/salazar/university/Client.java
@@ -2,6 +2,7 @@ package ir.salazar.university;
 
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -9,19 +10,38 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Client {
 
 	public static Lock lock;
+	public static ArrayList<Socket> serverTunnelSockets;
+	public static ServerSocket accepterSocket = null;
+	public static String name = "";
 
 	public static void main(String[] args) {
+		Client.serverTunnelSockets = new ArrayList<Socket>();
 		Client.lock = new ReentrantLock();
 		Socket clientSocket = null;
-		DataOutputStream os = null;
+		PrintStream os = null;
 		BufferedReader is = null;
+		Client.name = "Jamile";
 		try {
 			clientSocket = new Socket("localhost", 2013);
-			os = new DataOutputStream(clientSocket.getOutputStream());
+			os = new PrintStream(clientSocket.getOutputStream());
 			is = new BufferedReader(new InputStreamReader(
 					clientSocket.getInputStream()));
+			System.out
+					.println("ServerTunnel in client is running and kickin on port 2014");
 			if (clientSocket != null && os != null && is != null)
 				new ClientTunnel(clientSocket, os, is);
+
+			try {
+				accepterSocket = new ServerSocket(2014);
+				Socket tmpSocket = null;
+				while ((tmpSocket = accepterSocket.accept()) != null) {
+					serverTunnelSockets.add(tmpSocket);
+					new ServerTunnel(tmpSocket);
+				}
+			} catch (IOException e) {
+				System.out.println(e);
+			}
+
 		} catch (UnknownHostException e) {
 			System.err.println("Don't know about host: hostname");
 		} catch (IOException e) {
@@ -29,18 +49,16 @@ public class Client {
 					.println("Couldn't get I/O for the connection to: hostname");
 		}
 	}
-
 }
 
 class ClientTunnel implements Runnable {
 
 	private Socket clientSocket;
-	private DataOutputStream os;
+	private PrintStream os;
 	private BufferedReader is;
 	Thread thread;
 
-	public ClientTunnel(Socket clientSocket, DataOutputStream os,
-			BufferedReader is) {
+	public ClientTunnel(Socket clientSocket, PrintStream os, BufferedReader is) {
 		this.clientSocket = clientSocket;
 		this.os = os;
 		this.is = is;
@@ -52,21 +70,32 @@ class ClientTunnel implements Runnable {
 	public void run() {
 		try {
 			Scanner sc = new Scanner(System.in);
-			while (sc.hasNext()) {
-				os.writeBytes(sc.nextLine() + "\n");
-				String responseLine;
-				if ((responseLine = is.readLine()) != null) {
-					System.out.println("Server: " + responseLine);
-					if (responseLine.indexOf("Ok") != -1)
-						break;
-					if (responseLine.toLowerCase().startsWith("exit"))
-						break;
-				}
-			}
+			os.println(Client.name);
+			String responseLine;
+			if ((responseLine = is.readLine()) != null)
+				if (responseLine.equals(Client.name)) {
+					System.out.println("Connected to the host");
+					while (sc.hasNext()) {
+						os.println(sc.nextLine());
+						if ((responseLine = is.readLine()) != null) {
+							if (!responseLine.equals("exit"))
+								System.out.println("Server: " + responseLine);
+							if (responseLine.toLowerCase().equals("exit")
+									|| responseLine.toLowerCase().equals(
+											"killall")) {
+								for (Socket s : Client.serverTunnelSockets)
+									s.close();
+								break;
+							}
+						}
+					}
+				} else
+					System.err.println("Failed to send my name to host");
 			sc.close();
 			os.close();
 			is.close();
 			clientSocket.close();
+			Client.accepterSocket.close();
 		} catch (UnknownHostException e) {
 			System.err.println("Trying to connect to unknown host: " + e);
 		} catch (IOException e) {
@@ -78,11 +107,11 @@ class ClientTunnel implements Runnable {
 class ServerTunnel implements Runnable {
 
 	private Thread thread;
-	private Socket socket;
+	private Socket serverSocket;
 
 	public ServerTunnel(Socket socket) {
 		Client.lock.lock();
-		this.socket = socket;
+		this.serverSocket = socket;
 		this.thread = new Thread(this, "Client connection to server");
 		this.thread.start();
 		Client.lock.unlock();
@@ -95,23 +124,16 @@ class ServerTunnel implements Runnable {
 		PrintStream os;
 		try {
 			is = new BufferedReader(new InputStreamReader(
-					this.socket.getInputStream()));
-			os = new PrintStream(this.socket.getOutputStream());
-			while (!this.socket.isClosed()) {
+					this.serverSocket.getInputStream()));
+			os = new PrintStream(this.serverSocket.getOutputStream());
+			while (!this.serverSocket.isClosed()) {
 				line = is.readLine();
-				os.println(line);
-				if (line.toLowerCase().startsWith("exit")) {
-					this.socket.close();
-					if (line.toLowerCase().equals("exitall")) {
-						System.out.println("Killing server now...");
-						for (ClientData client : Server.clients)
-							client.getSocket().close();
-						Server.serverSocket.close();
-					}
-				}
+				os.println("Server said: " + line);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			Client.serverTunnelSockets.remove(this.serverSocket);
 		}
 	}
 }
